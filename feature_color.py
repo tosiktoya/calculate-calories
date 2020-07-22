ddd89a3bda15404ea5f8aae83c15262a4f0c09c2
tosiktoya <49915027+tosiktoya@users.noreply.github.com>
2020-07-22 17:57:47 +0900

    v2

--- feature_color.py
+++ feature_color.py
@@ -15,15 +15,18 @@ def getColorFeature(img):
     featurevec = []
 
     # 이미지 히스토그램 생성
-    # 3d channel, H : 0~180, S : 0~255, V : 0~255
+    # H : 0~180, S : 0~255, V : 0~255
+    # cv2.calcHist(img, channel, mask, histSize, ranges)
+
     hist = cv2.calcHist([img_hsv], [0, 1, 2], None, [6, 2, 2], [0, 180, 0, 256, 0, 256])
     for i in range(6):
         for j in range(2):
             for k in range(2):
                 featurevec.append(hist[i][j][k])
 
-    # 정규화
     feature = featurevec[1:]
+
+    # 정규화
     M = max(feature)
     m = min(feature)
     feature = list(map(lambda x: x * 2, feature))
@@ -35,8 +38,8 @@ def getColorFeature(img):
     return feature
 
 if __name__ == '__main__':
-    img = cv2.imread(sys.argv[1])
-    #img = cv2.imread("./images/All_Images/1_1.jpg", cv2.IMREAD_COLOR)
+    #img = cv2.imread(sys.argv[1])
+    img = cv2.imread("./images/All_Images/1_1.jpg", cv2.IMREAD_COLOR)
     featureVector = getColorFeature(img)
     #print(featureVector)
     cv2.waitKey(0)
