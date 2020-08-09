5ba86a44ed747bf9286ddec83baa07b87d742bd4
byeongjun96 <61685101+byeongjun96@users.noreply.github.com>
2020-08-09 16:22:36 +0900

    exe.py 수정사항

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 import numpy as np
 import cv2
 from create_feature import *
@@ -22,48 +23,41 @@ def output(path):
     skin_areas = []
     fruit_calories_100grams = []
 
-    for j in [1]:
-        for i in range(21, 22):
-            img_path = path
-            print("이미지경로 : " , img_path)
-            fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg(img_path)
-            pix_cm.append(pix_to_cm)
-            fruit_contours.append(fcont)
-            fruit_areas.append(farea)
-            feature_mat.append(fea)
-            skin_areas.append(skinarea)
-            response.append([float(j)])
-            image_names.append(img_path)
+    img_path = path
+    print("이미지경로 : " , img_path)
+    fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg(img_path)
+    pix_cm.append(pix_to_cm)
+    fruit_contours.append(fcont)
+    fruit_areas.append(farea)
+    feature_mat.append(fea)
+    skin_areas.append(skinarea)
+    image_names.append(img_path)
 
     testData = np.array(feature_mat, dtype=np.float32).reshape(-1, 94)
-    responses = np.array(response, dtype=int)
     result = svm_model.predict(testData)[1]
     result = np.ravel(result)
-    mask = result == responses
-    #print(result)
 
     # calculate calories
-    for i in range(0, len(result)):
-        #print(result[i])
-        volume = getVolume(result[i], fruit_areas[i], skin_areas[i], pix_cm[i], fruit_contours[i])
-        mass, cal, cal_100 = getCalorie(result[i], volume)
-        fruit_volumes.append(volume)
-        fruit_calories.append(cal)
-        fruit_calories_100grams.append(cal_100)
-        fruit_mass.append(mass)
+    volume = getVolume(result[0], fruit_areas[0], skin_areas[0], pix_cm[0], fruit_contours[0])
+    mass, cal, cal_100 = getCalorie(result[0], volume)
+    fruit_volumes.append(volume)
+    fruit_calories.append(cal)
+    fruit_calories_100grams.append(cal_100)
+    fruit_mass.append(mass)
 
-        #print(' i and result', i, result)
-
-    for i in range(0, len(mask)):
-        if mask[i][0] == False:
-            print( result[i], image_names[i])
-
-    correct = np.count_nonzero(mask)
-    #print(correct * 100.0 / result.size)
+    print("예측레이블 : ", result[0])
+    print("예측과일 : 나중에 if문 하면 나옴")
+    print("예측부피 : ", mass)
+    print("예측칼로리 : ", cal)
+    print("100g당칼로리: ", cal_100)
 
 if __name__ == '__main__':
-    # 경로설정
-    #path = './images/Test_Images/1_21.jpg'
-    path = "C:/Calorie Estimation/images/Apple/1_1.jpg"
+    # 사용자가 이미지 업로드 하면 이미지파일 경로획득
+    # 이미지파일의 해상도,크기조절 해야할듯?
+
+    path = "C:/Calorie Estimation/images/All_Images/1_1.jpg"
+
+    # path 획득후 output 돌리면
+    # 출력해야할것 : 사용자가 입력한 이미지, 음식이름, 칼로리, 추가정보 등등 ?
 
     output(path)
