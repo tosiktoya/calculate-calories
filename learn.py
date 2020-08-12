11e933d8071de13ee6978be224ace62648f825f0
byeongjun96 <61685101+byeongjun96@users.noreply.github.com>
2020-08-12 18:59:54 +0900

    v1.0

---
+++ learn.py
@@ -0,0 +1,105 @@
+import numpy as np
+import cv2
+from create_feature import *
+from calorie_calc import *
+import csv
+
+svm_params = dict( kernel_type = cv2.ml.SVM_LINEAR, svm_type = cv2.ml.SVM_C_SVC, C=2.67, gamma=5.383 )
+
+
+def training():
+	feature_mat = []
+	response = []
+	for j in [1,2]:
+		for i in range(1,21):
+			print ("./images/All_Images/"+str(j)+"_"+str(i)+".jpg")
+			fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg("./images/All_Images/"+str(j)+"_"+str(i)+".jpg")
+			feature_mat.append(fea)
+			response.append(float(j))
+			
+
+	trainData = np.array(feature_mat,dtype = np.float32).reshape(-1,94)
+	responses = np.array(response, dtype = int)
+
+	print(trainData)
+	print(responses)
+
+	svm = cv2.ml.SVM_create()
+	svm.setKernel(cv2.ml.SVM_LINEAR)
+	svm.setType(cv2.ml.SVM_C_SVC)
+	svm.setC(2.67)
+	svm.setGamma(5.383)
+
+	svm.train(trainData,cv2.ml.ROW_SAMPLE,responses)
+	svm.save('svm_data.dat')	
+
+def testing():
+	#svm_model = cv2.ml.SVM_create()
+	svm_model = cv2.ml.SVM_load('svm_data.dat')
+	feature_mat = []
+	response = []
+	image_names = []
+	pix_cm = []
+	fruit_contours = []
+	fruit_areas = []
+	fruit_volumes = []
+	fruit_mass = []
+	fruit_calories = []
+	skin_areas = []
+	fruit_calories_100grams = []
+
+	for j in [1,2]:
+		for i in range(21,26):	
+			img_path = "./images/Test_Images/"+str(j)+"_"+str(i)+".jpg"
+			print (img_path)
+			fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg(img_path)
+			feature_mat.append(fea)
+			response.append([float(j)])
+
+	testData = np.array(feature_mat,dtype = np.float32).reshape(-1,94)
+	responses = np.array(response, dtype = int)
+	result = svm_model.predict(testData)[1]
+	print(result)
+	result = np.ravel(result)
+	mask = result==responses
+	print(result)
+
+	#calculate calories
+	for i in range(0, len(result)):
+		print(result[i])
+		volume = getVolume(result[i], fruit_areas[i], skin_areas[i], pix_cm[i], fruit_contours[i])
+		mass, cal, cal_100 = getCalorie(result[i], volume)
+		fruit_volumes.append(volume)
+		fruit_calories.append(cal)
+		fruit_calories_100grams.append(cal_100)
+		fruit_mass.append(mass)
+		print(skin_areas[i])
+	
+
+	#write into csv file
+	with open('output.csv', 'w') as outfile:
+		writer = csv.writer(outfile)
+		data = ["Image name", "Desired response", "Output label", "Volume (cm^3)", "Mass (grams)", "Calories for food item", "Calories per 100 grams"]
+		writer.writerow(data)
+		for i in range(0, len(result)):
+			if (fruit_volumes[i] == None):
+				data = [str(image_names[i]), str(responses[i][0]), str(result[i][0]), "--", "--", "--", str(fruit_calories_100grams[i])]
+			else:
+				data = [str(image_names[i]), str(responses[i][0]), str(result[i][0]), str(fruit_volumes[i]), str(fruit_mass[i]), str(fruit_calories[i]), str(fruit_calories_100grams[i])]
+			writer.writerow(data)
+		outfile.close()
+	
+	for i in range(0, len(mask)):
+		if mask[i][0] == False:	
+			print ("(Actual Reponse)"), responses[i][0], "(Output)", result[i][0], image_names[i]
+
+	correct = np.count_nonzero(mask)
+	print (correct*100.0/result.size)
+	
+
+	
+
+if __name__ == '__main__':
+	training()
+	testing()
+
