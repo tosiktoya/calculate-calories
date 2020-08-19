6f274c5e6592375dff21355fd6eef8aed0c1bb55
tosiktoya <49915027+tosiktoya@users.noreply.github.com>
2020-08-19 19:16:17 +0900

    Add files via upload

--- calorie_calc.py
+++ calorie_calc.py
@@ -1,55 +1,106 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 import sys
 
-#density - gram / cm^3
-density_dict = {0:0.5, 1:0.609, 2:0.94, 3:0.577, 4:0.641, 5:1.151, 6:0.482, 7:0.513, 8:0.641, 9:0.481, 10:0.641, 11:0.521, 12:0.881, 13:0.228, 14:0.650 }
-#kcal
-calorie_dict = {0:50, 1:52, 2:89, 3:92, 4:41, 5:360, 6:47, 7:40, 8:158, 9:18, 10:16, 11:50, 12:61, 13:31, 14:30 }
-#skin of photo to real multiplier
-skin_multiplier = 5*2.3
+# density - gram / cm^3
+density_dict = {0: 0.5, 1: 0.609, 2: 0.94, 3: 0.577, 4: 0.641, 5: 1.151, 6: 0.482, 7: 0.513,
+                8: 0.641, 9: 0.481, 10: 0.641, 11: 0.521, 12: 0.881, 13: 0.228, 14: 0.650,
+                15: 0.310, 16: 1.162, 17: 0.961, 18: 0.952, 19: 1.005, 20: 0.178}
+# kcal
+calorie_dict = {0: 50, 1: 52, 2: 88, 3: 347, 4: 41, 5: 402, 6: 47, 7: 39,
+                8: 131, 9: 17, 10: 16, 11: 218, 12: 60, 13: 39, 14: 30,
+                15: 452, 16: 155, 17: 28, 18: 57, 19: 46, 20: 66}
 
-def getCalorie(label, volume): #volume in cm^3
-	'''
-	Inputs are the volume of the foot item and the label of the food item
-	so that the food item can be identified uniquely.
-	The calorie content in the given volume of the food item is calculated.
-	'''
-	calorie = calorie_dict[int(label)]
-	if (volume == None):
-		return None, None, calorie
-	density = density_dict[int(label)]
-	mass = volume*density*1.0
-	calorie_tot = (calorie/100.0)*mass
-	return mass, calorie_tot, calorie #calorie per 100 grams
+# carbohydrate
+carbohydrate_dict = {0: 5, 1: 14, 2: 23, 3: 63, 4: 10, 5: 1.3, 6: 12, 7: 9,
+                     8: 25, 9: 3.9, 10: 3.6, 11: 6, 12: 15, 13: 9, 14: 8,
+                     15: 51, 16: 1.1, 17: 9, 18: 15, 19: 11.4, 20: 12.65}
+
+# protein
+protein_dict = {0: 0.5, 1: 0.3, 2: 1.1, 3: 21, 4: 0.9, 5: 25, 6: 0.9, 7: 1.1,
+                8: 5, 9: 0.9, 10: 0.7, 11: 15, 12: 1.1, 13: 1.9, 14: 0.6,
+                15: 4.9, 16: 13, 17: 1.1, 18: 0.4, 19: 0.7, 20: 1.91}
+
+# fat
+fat_dict = {0: 0.5, 1: 0.2, 2: 0.3, 3: 1.2, 4: 0.2, 5: 33, 6: 0.1, 7: 0.1,
+            8: 1.1, 9: 0.2, 10: 0.11, 11: 15, 12: 0.5, 13: 0.4, 14: 0.2,
+            15: 25, 16: 11, 17: 0.3, 18: 0.1, 19: 0.3, 20: 0.82}
+
+# cholesterol
+cholesterol_dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 105, 6: 0, 7: 0,
+                    8: 33, 9: 0, 10: 0, 11: 2, 12: 0, 13: 0, 14: 0,
+                    15: 19, 16: 373, 17: 0, 18: 0, 19: 0, 20: 0}
+
+# Natrium
+natrium_dict = {0: 1, 1: 1, 2: 1, 3: 12, 4: 69, 5: 621, 6: 0, 7: 4,
+                8: 6, 9: 5, 10: 0, 11: 532, 12: 3, 13: 9, 14: 1,
+                15: 326, 16: 124, 17: 2, 18: 1, 19: 2, 20: 170}
+
+# skin of photo to real multiplier
+skin_multiplier = 5 * 2.3
+
+def getCalorie(label, volume):  # volume in cm^3
+    '''
+    Inputs are the volume of the foot item and the label of the food item
+    so that the food item can be identified uniquely.
+    The calorie content in the given volume of the food item is calculated.
+    '''
+    calorie_100 = calorie_dict[int(label)]
+    carbohydrate_100 = carbohydrate_dict[int(label)]
+    protein_100 = protein_dict[int(label)]
+    fat_100 = fat_dict[int(label)]
+    cholesterol_100 = cholesterol_dict[int(label)]
+    natrium_100 = natrium_dict[int(label)]
+
+    if (volume == None):
+        return None, None, calorie_100, carbohydrate_100, protein_100, fat_100, cholesterol_100, natrium_100
+    density = density_dict[int(label)]
+    mass = volume * density * 1.0
+    calorie_tot = (calorie_100 / 100.0) * mass
+
+    mass = round(mass, 2)
+    calorie_tot = round(calorie_tot, 2)
+
+    # calories and nutrients per 100 grams
+    return mass, calorie_tot, calorie_100, carbohydrate_100, protein_100, fat_100, cholesterol_100, natrium_100
+
 
 def getVolume(label, area, skin_area, pix_to_cm_multiplier, fruit_contour):
-	'''
-	Using callibration techniques, the volume of the food item is calculated using the
-	area and contour of the foot item by comparing the foot item to standard geometric shapes
+    '''
+    Using callibration techniques, the volume of the food item is calculated using the
+    area and contour of the foot item by comparing the food item to standard geometric shapes
+    '''
 
-	'''
-	print('get Volume')
-	print(label)
-	area_fruit = (area/skin_area)*skin_multiplier #area in cm^2
-	
-	label = int(label)
-	volume = 100
-	if label == 1 or label == 9 or label == 7 or label == 6 or label==12: #sphere-apple,tomato,orange,kiwi,onion
-		radius = np.sqrt(area_fruit/np.pi)
-		volume = (4/3)*np.pi*radius*radius*radius
-		print (area_fruit, radius, volume, skin_area)
-	
-	if label == 2 or label == 10 or (label == 4 and area_fruit > 30): #cylinder like banana, cucumber, carrot
-		fruit_rect = cv2.minAreaRect(fruit_contour)
-		height = max(fruit_rect[1])*pix_to_cm_multiplier
-		radius = area_fruit/(2.0*height)
-		volume = np.pi*radius*radius*height
-		
-	if (label==4 and area_fruit < 30) or (label==5) or (label==11): #cheese, carrot, sauce
-		volume = area_fruit*0.5 #assuming width = 0.5 cm
+    area_fruit = (area / skin_area) * skin_multiplier  # area in cm^2
 
-	if (label==8) or (label==14) or (label==3) or (label==13):
-		volume = None
-	
-	return volume
+    label = int(label)
+    volume = 100
+
+    # sphere-apple,tomato,orange,kiwi,onion
+    if label == 1 or label == 9 or label == 7 or label == 6 or label == 12 or label == 18 or label == 19:
+        radius = np.sqrt(area_fruit / np.pi)
+        volume = (4 / 3) * np.pi * radius * radius * radius
+
+    # cylinder like banana, cucumber, carrot
+    if label == 2 or label == 10 or (label == 4 and area_fruit > 30):
+        fruit_rect = cv2.minAreaRect(fruit_contour)
+        height = max(fruit_rect[1]) * pix_to_cm_multiplier
+        radius = area_fruit / (2.0 * height)
+        volume = np.pi * radius * radius * height
+    # cheese, carrot, sauce, bread
+    if (label == 4 and area_fruit < 30) or (label == 5) or (label == 11) or (label == 15) or (label == 20):
+        volume = area_fruit * 0.5  # assuming width = 0.5 cm
+
+    # pasta, watermelon, beans, capsicum
+    if (label == 8) or (label == 14) or (label == 3) or (label == 13):
+        volume = None
+
+    # egg, lemon
+    if (label == 16) or (label == 17):
+        fruit_rect = cv2.minAreaRect(fruit_contour)
+        height = max(fruit_rect[1]) * pix_to_cm_multiplier
+        radius = area_fruit / (2.0 * height)
+        volume = np.pi * radius * radius * height * 0.6
+
+    return volume
--- learn.py
+++ learn.py
@@ -1,105 +1,99 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import cv2
 from create_feature import *
 from calorie_calc import *
 import csv
 
-svm_params = dict( kernel_type = cv2.ml.SVM_LINEAR, svm_type = cv2.ml.SVM_C_SVC, C=2.67, gamma=5.383 )
-
+svm_params = dict(kernel_type=cv2.ml.SVM_LINEAR, svm_type=cv2.ml.SVM_C_SVC, C=2.67, gamma=5.383)
 
 def training():
-	feature_mat = []
-	response = []
-	for j in [1,2]:
-		for i in range(1,21):
-			print ("./images/All_Images/"+str(j)+"_"+str(i)+".jpg")
-			fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg("./images/All_Images/"+str(j)+"_"+str(i)+".jpg")
-			feature_mat.append(fea)
-			response.append(float(j))
-			
+    feature_mat = []
+    response = []
+    for j in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]:
+    #for j in [1, 2]:
+        for i in range(1, 21):
+            print("./images/All_Images/" + str(j) + "_" + str(i) + ".jpg")
+            fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg(
+                "./images/All_Images/" + str(j) + "_" + str(i) + ".jpg")
+            feature_mat.append(fea)
+            response.append(float(j))
 
-	trainData = np.array(feature_mat,dtype = np.float32).reshape(-1,94)
-	responses = np.array(response, dtype = int)
+    trainData = np.array(feature_mat, dtype=np.float32).reshape(-1, 94)
+    responses = np.array(response, dtype=int)
 
-	print(trainData)
-	print(responses)
+    svm = cv2.ml.SVM_create()
+    svm.setKernel(cv2.ml.SVM_LINEAR)
+    svm.setType(cv2.ml.SVM_C_SVC)
+    svm.setC(2.67)
+    svm.setGamma(5.383)
 
-	svm = cv2.ml.SVM_create()
-	svm.setKernel(cv2.ml.SVM_LINEAR)
-	svm.setType(cv2.ml.SVM_C_SVC)
-	svm.setC(2.67)
-	svm.setGamma(5.383)
-
-	svm.train(trainData,cv2.ml.ROW_SAMPLE,responses)
-	svm.save('svm_data.dat')	
+    svm.train(trainData, cv2.ml.ROW_SAMPLE, responses)
+    svm.save('svm_data.dat')
 
 def testing():
-	#svm_model = cv2.ml.SVM_create()
-	svm_model = cv2.ml.SVM_load('svm_data.dat')
-	feature_mat = []
-	response = []
-	image_names = []
-	pix_cm = []
-	fruit_contours = []
-	fruit_areas = []
-	fruit_volumes = []
-	fruit_mass = []
-	fruit_calories = []
-	skin_areas = []
-	fruit_calories_100grams = []
+    # svm_model = cv2.ml.SVM_create()
+    svm_model = cv2.ml.SVM_load('svm_data.dat')
+    feature_mat = []
+    response = []
+    image_names = []
+    pix_cm = []
+    fruit_contours = []
+    fruit_areas = []
+    fruit_volumes = []
+    fruit_mass = []
+    fruit_calories = []
+    skin_areas = []
+    fruit_calories_100grams = []
+    for j in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]:
+        for i in range(21, 26):
+            img_path = "./images/Test_Images/" + str(j) + "_" + str(i) + ".jpg"
+            print(img_path)
+            fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg(img_path)
+            pix_cm.append(pix_to_cm)
+            fruit_contours.append(fcont)
+            fruit_areas.append(farea)
+            feature_mat.append(fea)
+            skin_areas.append(skinarea)
+            response.append([float(j)])
+            image_names.append(img_path)
 
-	for j in [1,2]:
-		for i in range(21,26):	
-			img_path = "./images/Test_Images/"+str(j)+"_"+str(i)+".jpg"
-			print (img_path)
-			fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg(img_path)
-			feature_mat.append(fea)
-			response.append([float(j)])
+    testData = np.array(feature_mat, dtype=np.float32).reshape(-1, 94)
+    responses = np.array(response, dtype=int)
+    result = svm_model.predict(testData)[1]
+    result = np.ravel(result)
+    mask = result == responses
 
-	testData = np.array(feature_mat,dtype = np.float32).reshape(-1,94)
-	responses = np.array(response, dtype = int)
-	result = svm_model.predict(testData)[1]
-	print(result)
-	result = np.ravel(result)
-	mask = result==responses
-	print(result)
+    # calculate calories
+    for i in range(0, len(result)):
+        volume = getVolume(result[i], fruit_areas[i], skin_areas[i], pix_cm[i], fruit_contours[i])
+        mass, cal, cal_100, carbo_100, pro_100, fat_100, choles_100, nat_100 = getCalorie(result[i], volume)
+        fruit_volumes.append(volume)
+        fruit_calories.append(cal)
+        fruit_calories_100grams.append(cal_100)
+        fruit_mass.append(mass)
 
-	#calculate calories
-	for i in range(0, len(result)):
-		print(result[i])
-		volume = getVolume(result[i], fruit_areas[i], skin_areas[i], pix_cm[i], fruit_contours[i])
-		mass, cal, cal_100 = getCalorie(result[i], volume)
-		fruit_volumes.append(volume)
-		fruit_calories.append(cal)
-		fruit_calories_100grams.append(cal_100)
-		fruit_mass.append(mass)
-		print(skin_areas[i])
-	
+    # write into csv file
+    with open('output.csv', 'w') as outfile:
+        writer = csv.writer(outfile)
+        data = ["Image name", "Desired response", "Output label", "Volume (cm^3)", "Mass (grams)",
+                "Calories for food item", "Calories per 100 grams"]
+        writer.writerow(data)
+        for i in range(0, len(result)):
+            if (fruit_volumes[i] == None):
+                data = [str(image_names[i]), str(responses[i][0]), str(result[i]), "--", "--", "--",
+                        str(fruit_calories_100grams[i])]
+            else:
+                data = [str(image_names[i]), str(responses[i][0]), str(result[i]), str(fruit_volumes[i]),
+                        str(fruit_mass[i]), str(fruit_calories[i]), str(fruit_calories_100grams[i])]
+            writer.writerow(data)
+        outfile.close()
 
-	#write into csv file
-	with open('output.csv', 'w') as outfile:
-		writer = csv.writer(outfile)
-		data = ["Image name", "Desired response", "Output label", "Volume (cm^3)", "Mass (grams)", "Calories for food item", "Calories per 100 grams"]
-		writer.writerow(data)
-		for i in range(0, len(result)):
-			if (fruit_volumes[i] == None):
-				data = [str(image_names[i]), str(responses[i][0]), str(result[i][0]), "--", "--", "--", str(fruit_calories_100grams[i])]
-			else:
-				data = [str(image_names[i]), str(responses[i][0]), str(result[i][0]), str(fruit_volumes[i]), str(fruit_mass[i]), str(fruit_calories[i]), str(fruit_calories_100grams[i])]
-			writer.writerow(data)
-		outfile.close()
-	
-	for i in range(0, len(mask)):
-		if mask[i][0] == False:	
-			print ("(Actual Reponse)"), responses[i][0], "(Output)", result[i][0], image_names[i]
-
-	correct = np.count_nonzero(mask)
-	print (correct*100.0/result.size)
-	
-
-	
+    for i in range(0, len(mask)):
+        if mask[i][0] == False:
+            print("(Actual Reponse)", responses[i][0], "(Output)", result[i], image_names[i])
 
 if __name__ == '__main__':
-	training()
-	testing()
+    training()
+    #testing()
 
