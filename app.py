a02e89ab6a014f1f25db9688b0ce7ef2aa323dda
tosiktoya <49915027+tosiktoya@users.noreply.github.com>
2020-08-19 19:21:45 +0900

    8.19 app.py, calc_cal.py, learn.py 3개 코드 수정

--- app.py
+++ app.py
@@ -17,13 +17,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-#메인 페이지
-@app.route('/')
-def render_file():
-    return render_template('index.html')
-
-
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload():
     if request.method == 'GET':
         return render_template('upload.html')
@@ -51,9 +45,14 @@ def upload():
                 12 : 'Kiwi',
                 13 : 'Capsicum',
                 14 : 'Watermelon',
+                15 : 'Doughnut',
+                16 : 'Egg',
+                17 : 'Lemon',
+                18 : 'Pear',
+                19 : 'Plum',
+                20 : 'Bread'
                 }
 
-            
             image_name = f.filename
             basepath = os.path.dirname(__file__)
             file_path = os.path.join(
@@ -64,13 +63,8 @@ def upload():
             pix_cm = []
             fruit_contours = []
             fruit_areas = []
-            fruit_volumes = []
-            fruit_mass = []
-            fruit_calories = []
-            fruit_calories_100grams = []
             skin_areas = []
             feature_mat = []
-            #image_names = []
 
             fea, farea, skinarea, fcont, pix_to_cm = readFeatureImg("./static/"+f.filename)
             pix_cm.append(pix_to_cm)
@@ -78,22 +72,18 @@ def upload():
             fruit_areas.append(farea)
             feature_mat.append(fea)
             skin_areas.append(skinarea)
-            #image_names.append(img_path)
+
             testData = np.array(feature_mat, dtype=np.float32).reshape(-1, 94)
             result = svm_model.predict(testData)[1]
             result = np.ravel(result)
 
             # calculate calories
             volume = getVolume(result[0], fruit_areas[0], skin_areas[0], pix_cm[0], fruit_contours[0])
-            mass, cal, cal_100 = getCalorie(result[0], volume)
-            fruit_volumes.append(volume)
-            fruit_calories.append(cal)
-            fruit_calories_100grams.append(cal_100)
-            fruit_mass.append(mass)
+            mass, cal, cal_100, carbo_100, pro_100, fat_100, choles_100, nat_100 = getCalorie(result[0], volume)
 
-            #image = process_image(file_path)
             class_name = classes[result[0]]
-            return render_template('upload.html', label = class_name, img = image_name, mass = mass, cal = cal, cal_100 = cal_100)
+            return render_template('upload.html', label=class_name, img=image_name, mass=mass, cal=cal, cal_100=cal_100,
+                                   carbo_100=carbo_100, pro_100=pro_100, fat_100=fat_100, choles_100=choles_100, nat_100=nat_100)
     return
 
 if __name__ == '__main__':
